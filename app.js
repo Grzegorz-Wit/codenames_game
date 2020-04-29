5cb80ca520c1a344ec37953320e91be3efdb1cd3
Grzegorz-Wit <grzegorz133@gmail.com>
2020-04-29 12:38:51 +0200

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express, { static } from 'express';
+var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client',static(__dirname + '/client'));
+app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen();
 console.log('server started')
